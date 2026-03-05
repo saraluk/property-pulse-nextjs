78a3726add34131fd5560034ce0b809dcc2f5805
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-03-05 19:50:03 +0700

    map display

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -7,6 +7,7 @@ import {
   FaCheck,
   FaMapMarker,
 } from "react-icons/fa";
+import { PropertyMap } from "./PropertyMap";
 
 export default function PropertyDetails({ property }) {
   return (
@@ -93,7 +94,7 @@ export default function PropertyDetails({ property }) {
       </div>
       {/* <!-- Map --> */}
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
+        <PropertyMap property={property} />
       </div>
     </main>
   );
---
+++ components/PropertyMap.jsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { setDefaults, fromAddress } from "react-geocode";
+import { useEffect, useState } from "react";
+import Map, { Marker } from "react-map-gl/mapbox";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Image from "next/image";
+import pin from "@/assets/images/pin.svg";
+import Spinner from "./Spinner";
+
+export function PropertyMap({ property }) {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [viewport, setViewport] = useState({
+    latitude: 0,
+    longitude: 0,
+    zoom: 12,
+    width: "100%",
+    height: "500px",
+  });
+  const [loading, setLoading] = useState(true);
+  const [geocodeError, setGeocodeError] = useState(false);
+
+  setDefaults({
+    key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
+    language: "en",
+    region: "us",
+  });
+
+  useEffect(() => {
+    const fetchCoords = async () => {
+      try {
+        const res = await fromAddress(
+          `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`,
+        );
+
+        // Check geocode results
+        if (res.results.length === 0) {
+          setGeocodeError(true);
+          return;
+        }
+
+        const { lat, lng } = res.results[0].geometry.location;
+        setLat(lat);
+        setLng(lng);
+        setViewport({
+          ...viewport,
+          latitude: lat,
+          longitude: lng,
+        });
+      } catch (error) {
+        console.log(error);
+        setGeocodeError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoords();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (geocodeError) {
+    return <div className="text-xl">No location data found.</div>;
+  }
+
+  return (
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 15,
+        }}
+        style={{ width: "100%", height: 500 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={lng} latitude={lat} anchor="bottom">
+          <Image src={pin} alt="location" width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
+}
