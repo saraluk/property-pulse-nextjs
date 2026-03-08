4d5dd90a516098996f96dfc5182e8a80df265ba8
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-03-08 20:21:55 +0700

    bookmark property action

---
+++ app/actions/bookmarkProperty.js
@@ -0,0 +1,40 @@
+"use server";
+
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+
+export default async function bookmarkProperty(propertyId) {
+  await connectDB();
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required.");
+  }
+
+  const { userId } = sessionUser;
+  const user = User.findById(userId);
+
+  const isBookmarked = user.bookmarks.includes(propertyId);
+  let message;
+  if (isBookmarked) {
+    // If already bookmarked, then remove
+    user.bookmarks.pull(propertyId);
+    message = "Bookmark removed";
+    isBookmarked = false;
+  } else {
+    // If not bookmarked, then add
+    user.bookmarks.push(propertyId);
+    message = "Bookmark added";
+    isBookmarked = true;
+  }
+
+  await user.save();
+  revalidatePath("/properties/saved", "page");
+
+  return {
+    message,
+    isBookmarked,
+  };
+}
