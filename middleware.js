6d56748e3da916fb6509489f6984727c9ae7df34
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-03-12 14:51:27 +0700

    attempt 2

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth();
 
 export const config = {
   // A list of protected routes that we don't want unauthenticated users to access
