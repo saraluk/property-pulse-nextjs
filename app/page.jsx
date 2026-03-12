b14a4e6d77b0e04b149ad33b3ed93d8ba29d025b
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-03-12 13:57:27 +0700

    featured properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
+import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </>
   );
