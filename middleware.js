70b8ebb0b802c554f3a776c65a7bf87812b28b08
Saraluk Kaiwansakul <saralukkai@gmail.com>
2026-03-12 14:54:46 +0700

    attempt 3

--- middleware.js
+++ middleware.js
@@ -1,6 +1,21 @@
-import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export default withAuth();
+export default async function middleware(req) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/api/auth/signin";
+    url.searchParams.set("callbackUrl", req.nextUrl.href);
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   // A list of protected routes that we don't want unauthenticated users to access
